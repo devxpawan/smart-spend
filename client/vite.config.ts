4e89e079d236089208a1cf4dd4eab336d7b116f8
Pawan Perera <pawanperera5@gmail.com>
2025-12-11 20:09:34 +0530

    Fix proxy target in vite.config.ts to point to localhost:5000

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,11 +8,11 @@ export default defineConfig({
     port: 5173,
     proxy: {
       "/api": {
-        target: "http://localhost:5001",
+        target: "http://localhost:5000",
         changeOrigin: true,
       },
       "/uploads": {
-        target: "http://localhost:5001",
+        target: "http://localhost:5000",
         changeOrigin: true,
       },
     },
